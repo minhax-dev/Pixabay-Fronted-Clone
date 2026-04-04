abfab6d8bd1c13136b0a021095c66f1c460d2682
Minhaj <minhaj.devx@gmail.com>
2026-04-04 08:32:58 +0530

    feat : add tailwindCss dependency and basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import Navbar from './components/layouts/Navbar'
+import Categories from './components/layouts/Categories'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <div className="min-h-screen bg-zinc-900">
+        <Navbar />
+        <Categories />
+      </div>
     </>
 
   )
