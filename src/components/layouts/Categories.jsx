0d323b95c13097c38eca1c0b574570f237d1da03
Minhaj <minhaj.devx@gmail.com>
2026-04-05 22:58:11 +0530

    feat : Add Category Component

--- src/components/layouts/Categories.jsx
+++ src/components/layouts/Categories.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
 const Categories = () => {
-  return (
-    <div>
+  let categories = [
+    "Trending",
+    "Nature",
+    "Technology",
+    "People",
+    "Animals",
+    "Architecture",
+    "Travel",
+    "Food",
+    "Indian",
+    "Russian",
+  ];
 
+  return (
+    <div className='bg-black/30 backdrop-blur border-b border-white/5'>
+      <div className='max-w-7xl mx-auto px-8 py-2.5'>
+        <div className="flex gap-2 items-center justify-center overflow-x-auto">
+          {categories.map((ele) => (
+            <button key={ele}
+              onClick={() => console.log("Category Clicked..")}
+              className='text-white bg-zinc-700/50 px-4 py-2 text-sm rounded-lg cursor-pointer hover:scale-95 duration-150 ease-in-out '
+            > {ele}
+            </button>
+          ))}
+        </div>
+
+      </div>
     </div>
   )
 }
